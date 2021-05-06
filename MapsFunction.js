806fe69bd20362843e23bfa93db21a56639d103c
User <2176589@mail.ru>
2021-05-05 21:39:47 -0700

    Maps

--- MapsFunction.js
+++ MapsFunction.js
@@ -7,7 +7,11 @@
  */
 
 function gridMap(fn, a) {
-    return a.map(x => x.map(fn))
+    let c = []
+    for (let i = 0; i < a.length; i++) {
+        c.push(a[i].map(fn))
+    }
+    return c
 }
 
 console.log(gridMap(x => x + 1, [[1, 2, 3, 4], [5, 6, 7, 8, 9], [0, 2, 4]]))
